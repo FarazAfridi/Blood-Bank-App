219aa09d45be57b49ef57cf0aeb418b14acec8cb
FarazAfridi <farazafridi5159@gmail.com>
2021-01-21 00:34:52 +0500

    eject into react native cli

--- screens/DonorsPage.js
+++ screens/DonorsPage.js
@@ -1,42 +1,70 @@
-import React from 'react';
-import { useState } from 'react';
-import {Text, View, StyleSheet, Button, TextInput} from "react-native";
-import { FlatList } from 'react-native-gesture-handler';
-import { useSelector, useDispatch } from 'react-redux';
-import { addDonor } from './../store/action/bloodBankActions';
+import React from "react";
+import { useState } from "react";
+import { Text, View, StyleSheet, Button, TextInput, ScrollView } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { useSelector, useDispatch } from "react-redux";
+import { addDonor } from "./../store/action/bloodBankActions";
 
 const DonorsPage = () => {
-    const dispatch = useDispatch();
-    const donors = useSelector(state => state.blood.donors)
-    const [donorBloodGroup, setDonorBloodGroup] = useState('')
-    const [donorName, setDonorName] = useState('')
-    const id = Math.floor(Math.random() * 10000).toString()
+  const dispatch = useDispatch();
+  const donors = useSelector((state) => state.blood.donors);
+  const [donorBloodGroup, setDonorBloodGroup] = useState("");
+  const [donorName, setDonorName] = useState("");
+  const id = Math.floor(Math.random() * 10000).toString();
+  const [donorLocation, setDonorLocation] = useState("");
 
-    return ( 
-        <View>
-            <FlatList data={donors} renderItem={(itemData) => {
-                return <View style={styles.donorsContainer}>
-                    <Text>{itemData.item.name}</Text>
-                    <Text>{itemData.item.bloodGroup}</Text>
-                </View>
-            }} />
-            <View>
-            <TextInput placeholder="Blood Group" onChangeText={setDonorBloodGroup} />
-            <TextInput placeholder="Donor Name" onChangeText={setDonorName} />
-            <Button title="Add Donor" onPress={() => dispatch(addDonor({id ,name: donorName, bloodGroup: donorBloodGroup}))}/>
+  return (
+    <View>
+
+    <ScrollView>
+      <FlatList
+        data={donors}
+        renderItem={(itemData) => {
+          return (
+            <View style={styles.donorsContainer}>
+              <Text>{itemData.item.name}</Text>
+              <Text>{itemData.item.bloodGroup}</Text>
             </View>
-        </View>
-     );
-}
+          );
+        }}
+      />
+      </ScrollView>
+      <View>
+        <TextInput
+          placeholder="Blood Group"
+          onChangeText={setDonorBloodGroup}
+        />
+        <TextInput placeholder="Donor Name" onChangeText={setDonorName} />
+        <TextInput
+          placeholder="Donor Location"
+          onChangeText={setDonorLocation}
+        />
+        <Button
+          title="Add Donor"
+          onPress={() =>
+            dispatch(
+              addDonor({
+                id,
+                name: donorName,
+                bloodGroup: donorBloodGroup,
+                location: donorLocation,
+              })
+            )
+          }
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-donorsContainer: {
-    width: '300px',
-    height: '100px',
+  donorsContainer: {
+    width: 300,
+    height: 100,
     padding: 10,
-    justifyContent:'center',
-    alignItems: 'center',
-}
-})
- 
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
 export default DonorsPage;
--- screens/Homepage.js
+++ screens/Homepage.js
@@ -1,29 +1,68 @@
 import React from "react";
 import { useState } from "react";
-import { Button, View, Text, TextInput } from "react-native";
-import { useDispatch } from 'react-redux';
+import { Button, View, Text, TextInput, StyleSheet } from "react-native";
+import { useDispatch } from "react-redux";
 import { findDonor } from "../store/action/bloodBankActions";
+import { CheckBox } from "native-base";
 
 const Homepage = ({ navigation }) => {
-    const [bloodGroup, setBloodGroup] = useState('')
-    const [location, setLocation] = useState('')
-    const dispatch = useDispatch();
+  const [bloodGroup, setBloodGroup] = useState("");
+  const [location, setLocation] = useState("");
+  const [bg, setBg] = useState(false);
+  const dispatch = useDispatch();
 
   return (
     <View>
-    <TextInput style={{padding: '10px'}} placeholder="Blood Group" onChangeText={setBloodGroup} />
-    <TextInput style={{padding: '10px'}} placeholder="Location" onChangeText={setLocation}  />
-  
+      <View style={styles.Container}>
+        <View>
+          <Text>Blood Group :</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <View style={styles.input}>
+            <CheckBox checked={bg} onPress={() => setBg(!bg)} />
+            <Text style={styles.text}>A</Text>
+          </View>
+          <View style={styles.input}>
+            <CheckBox checked={bg} onPress={() => setBg(!bg)} />
+            <Text style={styles.text}>B</Text>
+          </View>
+          <View style={styles.input}>
+            <CheckBox checked={bg} onPress={() => setBg(!bg)} />
+            <Text style={styles.text}>AB</Text>
+          </View>
+        </View>
+      </View>
+
+      {/* <TextInput placeholder="Blood Group" onChangeText={setBloodGroup} />
+      <TextInput placeholder="Location" onChangeText={setLocation} /> */}
+
       <Button
         title="Move to Donors"
         onPress={() => {
-            navigation.navigate("Donors")
-            dispatch(findDonor({bloodGroup: bloodGroup, location}))
-            }}
+          navigation.navigate("Donors");
+          dispatch(findDonor({ bloodGroup: bloodGroup, location }));
+        }}
       />
-      
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  Container: {
+    width: 300,
+  },
+  inputContainer: {
+    flexDirection: "column",
+  },
+  text: {
+      margin: 10,
+      padding: 10
+  },
+  input: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10
+  }
+});
 
 export default Homepage;
